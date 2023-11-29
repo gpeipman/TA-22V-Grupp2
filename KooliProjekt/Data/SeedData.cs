bf56ce48cf4476f4b572118e51f75597016878ee
MaksimTeterin <maksim.padriku@gmail.com>
2023-11-29 20:59:43 +0200

    Added users with roles

--- KooliProjekt/Data/SeedData.cs
+++ KooliProjekt/Data/SeedData.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using static System.Formats.Asn1.AsnWriter;
-
+ 
 namespace KooliProjekt.Data
 {
     public static class SeedData
@@ -11,57 +11,63 @@ namespace KooliProjekt.Data
             AddUsers(userManager);
             AddEvent(context);
             AddRole(roleManager);
+            AddAdmin(userManager);
         }
-
-        
-
-        private static void AddUsers(UserManager<IdentityUser> userManager)
+ 
+        private static void AddEvent(RoleManager<IdentityRole> roleManager)
         {
-            var user = new IdentityUser();
-            user.UserName = "mina@example.com";
-            user.EmailConfirmed = true;
-            user.PasswordHash = userManager.PasswordHasher.HashPassword(user, "Polükas2013*");
-
-            userManager.CreateAsync(user).Wait();
+            throw new NotImplementedException();
         }
-
-        private static void AddEvent(ApplicationDbContext context)
-        {
-            if (context.Events.Count() > 0)
-            {
-                return;
-            }
-
-            var @event = new Event();
-            @event.event_name = "UN Summit";
-
-            context.Add(@event);
-            context.SaveChanges();
-        }
+ 
         private static void AddRole(RoleManager<IdentityRole> roleManager)
         {
             if (roleManager.Roles.Count() > 0)
             {
                 return;
             }
-
+ 
             var newRole = new IdentityRole();
             newRole.Name = "Admin";
             newRole.Id = "1";
-
+ 
             roleManager.CreateAsync(newRole).Wait();
         }
-
-        private static void AddAdmin(IdentityUserRole<IdentityRole> settingRoles) // Короче где-то в этом методе ошибка
+ 
+        private static void AddUsers(UserManager<IdentityUser> userManager)
         {
-
-            var newUserWithRole = new IdentityRole();
-            newUserWithRole.UserId = "5a2d63ae-774e-4175-aa63-e53b56bdb016";
-            newUserWithRole.RoleId = "1";
-
-            //settingRoles.CreateAsync(newUserWithRole).Wait();
-
-        } 
-
+            if (userManager.Users.Count() > 0)
+            {
+                return;
+            }
+            var user = new IdentityUser();
+            user.UserName = "mina@example.com";
+            user.EmailConfirmed = true;
+            user.PasswordHash = userManager.PasswordHasher.HashPassword(user, "Polükas2013*");
+            user.Id = "1";
+ 
+            userManager.CreateAsync(user).Wait();
+            userManager.AddToRoleAsync(user, "Admin");
+        }
+ 
+        private static void AddEvent(ApplicationDbContext context)
+        {
+            if (context.Events.Count() > 0)
+            {
+                return;
+            }
+ 
+            var @event = new Event();
+            @event.event_name = "UN Summit";
+ 
+            context.Add(@event);
+            context.SaveChanges();
+        }
+        private static void AddAdmin(UserManager<IdentityUser> userManager) // Короче где-то в этом методе ошибка
+        {
+ 
+            var user = userManager.FindByIdAsync("1").Result;
+ 
+            userManager.AddToRoleAsync(user, "Admin").GetAwaiter().GetResult();
+        }
     }
 }
