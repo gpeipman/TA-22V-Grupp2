46b21ca746779d6160febef9c0918efeb58e4d62
Gunnar Peipman <gpeipman@hotmail.com>
2024-01-26 15:10:14 +0200

    lisatud testide projekt

---
+++ KooliProjekt.UnitTests/ControllerTests/HomeControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace KooliProjekt.UnitTests.ControllerTests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_should_return_index_view()
+        {
+
+        }
+    }
+}
