0b8bbba2b9268c1965ca3537ad70b94bf9ed2400
maksimteterin <maksim.padriku@gmail.com>
2024-03-22 18:22:32 +0200

    Tests for post actions

--- KooliProjekt.IntegrationTests/PostTests.cs
+++ KooliProjekt.IntegrationTests/PostTests.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using KooliProjekt.Data;
 using KooliProjekt.IntegrationTests.Helpers;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 
@@ -13,7 +14,7 @@ namespace KooliProjekt.IntegrationTests
     public class PostTests : TestBase
     {
         [Fact]
-        public async Task Create_EndpointReturnRedirectForCorrectModel()
+        public async Task Event_Create_EndpointReturnRedirectForCorrectModel()
         {
             // Arrange
             AuthenticateAdmin();
@@ -21,7 +22,6 @@ namespace KooliProjekt.IntegrationTests
             {
                 AllowAutoRedirect = false
             });
-            int id = 1;
 
             var formValues = new Dictionary<string, string>
             {
@@ -48,7 +48,7 @@ namespace KooliProjekt.IntegrationTests
             Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
         }
         [Fact]
-        public async Task Create_EndpointReturnsOKForIncorrectModel()
+        public async Task Event_Create_EndpointReturnsOKForIncorrectModel()
         {
             // Arrange
             AuthenticateAdmin();
@@ -63,6 +63,65 @@ namespace KooliProjekt.IntegrationTests
 
             // Act
             var response = await client.PostAsync("/Event/Create/", content);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Event_details_Create_EndpointReturnRedirectForCorrectModel()
+        {
+            // Arrange
+            AuthenticateAdmin();
+            var client = Factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+
+            var @event = new Event { event_name = "TEST" };
+            var user = new IdentityUser { Id = "1" };
+            var context = GetDbContext();
+            await context.AddAsync(@event);
+            await context.SaveChangesAsync();
+
+            var formValues = new Dictionary<string, string>
+            {
+                { "user_Id", user.Id },
+                { "event_Id", @event.Id.ToString() },
+                { "is_payed", "false" },
+                //{ "Id", $"{id}" }
+            };
+
+
+
+
+            var content = new FormUrlEncodedContent(formValues);
+
+
+            // Act
+            var response = await client.PostAsync("/Event_details/Create/", content);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+        }
+        [Fact]
+        public async Task Event_details_Create_EndpointReturnsOKForIncorrectModel()
+        {
+            // Arrange
+            AuthenticateAdmin();
+            var client = Factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+
+            var formValues = new Dictionary<string, string>
+            {
+                { "user_Id", "123456789" }
+            };
+            var content = new FormUrlEncodedContent(formValues);
+
+            // Act
+            var response = await client.PostAsync("/Event_details/Create/", content);
 
             // Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
